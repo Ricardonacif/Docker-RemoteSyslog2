a800f8e6d698741cdfbfb4aa7e0cbb1b0db0ae70
Ricardo Nacif <nacif.ricardo@gmail.com>
2018-08-16 17:33:01 -0300

    env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,17 +12,22 @@ fi
 # when no arguments are passed set options from environment
 # otherwise append all arguments to the remote_syslog command
 if [[ -z ${1} ]]; then
-  echo 'here3'
+  echo 'Logging started! Here are the variables bellow:'
+  echo "SYSLOG_FILES:"
   echo $SYSLOG_FILES
+  echo "SYSLOG_HOSTNAME:"
   echo $SYSLOG_HOSTNAME
+  echo "SYSLOG_HOST:"
   echo $SYSLOG_HOST
+  echo "INSTANCE_ID:"
   echo $INSTANCE_ID
+  echo "SYSLOG_PORT:"
   echo $SYSLOG_PORT
-  
+
   exec /remote_syslog/remote_syslog -D --dest-host=${SYSLOG_HOST} --dest-port=${SYSLOG_PORT:-514} \
   	--tcp=${SYSLOG_TCP:-false} --tls=${SYSLOG_TLS:-false} --facility=${SYSLOG_FACILITY:-user} \
   	--severity=${SYSLOG_SEVERITY:-notice} --hostname=${SYSLOG_HOSTNAME}-${INSTANCE_ID} ${SYSLOG_FILES}
 else
-  echo 'here4'
+  echo 'Running syslog with args passed'
   exec /remote_syslog/remote_syslog -D $@
 fi
